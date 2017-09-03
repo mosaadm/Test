148029b52bfae98c0904a7ffebab11ef0ea6e561
Mohamed Mahmoud <m.mosaad1991@gmail.com>
2017-09-03 20:23:33 +0200

    Delete chrome.bat

--- chrome.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-bitsadmin.exe /transfer "JobName" https://raw.githubusercontent.com/mosaadm/Test/master/chrome_extension.zip  C:\Users\Administrator\Desktop\chrome_extension.zip
-cd "C:\Users\Administrator\Desktop\"
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('chrome_extension.zip', 'chrome'); }"
-ping 127.0.0.1 -n 6 > nul
